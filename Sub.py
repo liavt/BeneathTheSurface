a65f5e8bb5930fad147d3f1871b5ce2aaacfeba6
liavt <liavt@yahoo.com>
2017-02-03 09:00:14 -0600

    Updated for actual competition

--- Sub.py
+++ Sub.py
@@ -39,10 +39,14 @@ except serial.SerialException:
                                         ser = serial.Serial('COM5', timeout=None, baudrate=9600, rtscts=False,
                                                             dsrdtr=False);
 
+print("Initializing...");
 
 pygame.init()
+print("Initialized. Finding joystick...")
 joystick = pygame.joystick.Joystick(0)
 joystick.init()
+
+print("Started");
 
 keepAlive = True
 
@@ -58,40 +62,79 @@ while keepAlive:
     if event.type == pygame.QUIT:
         keepAlive = False
     elif event.type == pygame.JOYAXISMOTION:
-        val = joystick.get_axis(0)
-        if val < 0.05 and val > -0.05:
-            val = 0;
+        '''
+            f = front
+            forward
+            b = front
+            backward
+            s = front
+            stop
 
-        if val > 0.5:
-            val = b"r";
-        elif val < -0.5:
-            val = b"l"
+            r = right
+            forward
+            e = right
+            backward
+            o = right
+            stop
+
+            l = left
+            forward
+            t = left
+            backward
+            a = left
+            stop
+
+            u = top
+            forward
+            d = top
+            backward
+            n = top
+            stop
+            '''
+
+        forward = -joystick.get_axis(1)
+        if forward < 0.5 and forward > -0.5:
+            forward = 0;
+
+        side = joystick.get_axis(0)
+        if side < 0.5 and side > -0.5:
+            side = 0;
+
+        twist = joystick.get_axis(3);
+        if twist < 0.5 and twist > -0.5:
+            twist = 0;
+
+        print(forward)
+
+        if forward == 0 and side == 0 and twist != 0:
+            if twist < -0.5:
+                ser.write(b"srt");
+            elif twist > 0.5:
+                ser.write(b"sle");
+        elif forward != 0 and side == 0:
+            if forward > 0.5:
+                ser.write(b"frl");
+            elif forward < -0.5:
+                ser.write(b"bet");
+        elif forward != 0 and side != 0:
+            if forward > 0.5:
+                ser.write(b"f")
+            elif forward < -0.5:
+                ser.write(b"b")
+            if side < -0.5:
+                ser.write(b"ar")
+            elif side > 0.5:
+                ser.write(b"ol")
         else:
-            val = b"c"
+            ser.write(b"sao");
 
-        print("Sending: "+str(val));
 
-        ser.write(val)
-
-        val = joystick.get_axis(1)
-        if val < 0.05 and val > -0.05:
-            val = 0;
-
-        if val > 0.5:
-            val = b"f";
-        elif val < -0.5:
-            val = b"b"
-        else:
-            val = b"s"
-
-        print("Sending: " + str(val));
-
-        ser.write(val)
     elif event.type == pygame.JOYHATMOTION:
         val = joystick.get_hat(0)
         if val[1] == 1:
-            ser.write(b"u")
-        elif val[1] == -1:
             ser.write(b"d")
+        elif val[1] == -1:
+            ser.write(b"u")
         else:
+
             ser.write(b"n")
--- bts/bts.ino
+++ bts/bts.ino
@@ -1,9 +1,9 @@
 #include <AFMotor.h>
 
-AF_DCMotor front(1);
-AF_DCMotor side(2);
-AF_DCMotor top(3);
-AF_DCMotor bottom(4);
+AF_DCMotor front(2);
+AF_DCMotor right(4);
+AF_DCMotor left(3);
+AF_DCMotor top(1);
 
 const int ledPin=13;
 
@@ -11,10 +11,10 @@ void setup() {
  pinMode(ledPin,OUTPUT);
  Serial.begin(9600);
 
- side.run(RELEASE);
  front.run(RELEASE);
+ right.run(RELEASE);
+ left.run(RELEASE);
  top.run(RELEASE);
- bottom.run(RELEASE);
 }
 
 void runMotor(AF_DCMotor& mot, int input){
@@ -33,6 +33,10 @@ void runMotor(AF_DCMotor& mot, int input){
   
   int output = input.toInt();*/
   
+  if(input == 0){
+    mot.setSpeed(input);
+    mot.run(RELEASE);
+  }
   if(input < 0){
     mot.run(BACKWARD);
     mot.setSpeed(-input);  
@@ -46,27 +50,52 @@ void runMotor(AF_DCMotor& mot, int input){
 
 void loop() {  
   while(Serial.available() > 0){
-    int incomingByte = Serial.read();
-
-    if(incomingByte == 'f'){
+    int instruction = Serial.read();
+     
+    /*
+    f = front forward
+    b = front backward
+    s = front stop
+    
+    r = right forward
+    e = right backward
+    o = right stop
+    
+    l = left forward
+    t = left backward
+    a = left stop
+    
+    u = top forward
+    d = top backward
+    n = top stop
+    */
+    
+    if(instruction == 'f'){
       runMotor(front, 255);
-    }else if(incomingByte == 'b'){
-      runMotor(front, -254);  
-    }else if(incomingByte == 'r'){
-      runMotor(side, 255);
-    }else if(incomingByte == 'l'){
-      runMotor(side, -254);
-    }else if(incomingByte == 'c'){
-      runMotor(side, 0);
-    }else if(incomingByte == 's'){
+    }else if(instruction == 'b'){
+      runMotor(front, -254);
+    }else if(instruction == 's'){
       runMotor(front, 0);
-    }else if(incomingByte == 'u'){
+    }else if(instruction == 'r'){
+      runMotor(right, 255);
+    }else if(instruction == 'e'){
+      runMotor(right, -254);
+    }else if(instruction == 'o'){
+      runMotor(right, 0);
+    }else if(instruction == 'l'){
+      runMotor(left, -254);
+    }else if(instruction == 't'){
+      runMotor(left, 255);
+    }else if(instruction == 'a'){
+      runMotor(left, 0);
+    }else if(instruction == 'u'){
       runMotor(top, 255);
-    }else if(incomingByte == 'd'){
+    }else if(instruction == 'd'){
       runMotor(top, -254);
-    }else if(incomingByte == 'n'){
+    }else if(instruction == 'n'){
       runMotor(top, 0);
     }
+    
   }
 }
 
